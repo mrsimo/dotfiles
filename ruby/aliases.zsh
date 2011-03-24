8ebf512b1cb797b08d4c663fd8569a3e415daf3b
Albert Llop <mrsimo@gmail.com>
2011-03-24 10:55:17 +0100

    Extar aliases from work

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,1 +1,9 @@
 alias be='bundle exec'
+alias awesome="TESTOPTS='--runner=awesome'"
+alias solr="rake solr:start"
+alias tenv="RAILS_ENV=test"
+alias tsolr="tenv solr"
+alias rmate="mate ."
+
+alias start_mongo="mongod run --config /usr/local/Cellar/mongodb/1.6.5-x86_64/mongod.conf"
+
