2839741cb00c4b59b0003e6e24ec4c419f1cf265
Albert Llop <mrsimo@gmail.com>
2014-09-14 13:17:30 +0200

    delete-merged alias

--- git/aliases.bash
+++ git/aliases.bash
@@ -1,3 +1,5 @@
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias glc="git log --graph --color --date=relative --stat"
 alias gcm="g"
+alias delete-merged='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
+
