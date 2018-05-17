980738fb6110c50574992ecf42748ec2816c1126
Albert Llop <mrsimo@gmail.com>
2018-05-17 18:14:16 +0200

    Bankrupcy: some ruby stuff

--- ruby/aliases.bash
+++ ruby/aliases.bash
@@ -1,6 +1,3 @@
 alias be='bundle exec'
-alias bo='EDITOR=mvim bundle open'
-alias fr='foreman run'
-alias fre='foreman run bundle exec'
+alias bo='EDITOR="code -n" bundle open'
 
-alias start_mongo="mongod run --config /usr/local/Cellar/mongodb/1.6.5-x86_64/mongod.conf"
